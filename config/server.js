a42eaf2058b074c455436ed4059a46afb38bdaa2
oshri <54857364+oshri134@users.noreply.github.com>
2024-05-12 13:28:34 +0300

    adsasd

--- config/server.js
+++ config/server.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+
+console.log("APP_KEYS:", process.env.APP_KEYS); // Add this line to debug
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["yourKeyA", "yourKeyB"]),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
