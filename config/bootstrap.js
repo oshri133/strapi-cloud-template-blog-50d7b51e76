abd412a2c99893b21bde991fb640fce4763d849d
oshri <54857364+oshri134@users.noreply.github.com>
2024-05-12 15:41:53 +0300

    l;kkl;

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,12 +1,19 @@
+// config/functions/bootstrap.js
+
+const migratePreparationTime = require("../migratePreparationTime");
+
 module.exports = async ({ strapi }) => {
-  const migratePreparationTime = require("../migratePreparationTime");
-
-  // Ensure Strapi is initialized
-  if (!strapi) {
-    console.error("Strapi instance is undefined");
-    return;
-  }
-
-  // Call the migration script
-  await migratePreparationTime({ strapi });
-};
+    if (!strapi) {
+      console.error('Strapi instance is undefined');
+      return;
+    }
+  
+    console.log('Strapi instance is initialized');
+  
+    try {
+      // Call the migration script with the strapi instance
+      await migratePreparationTime({ strapi });
+    } catch (error) {
+      console.error('Error during migration:', error);
+    }
+  };
